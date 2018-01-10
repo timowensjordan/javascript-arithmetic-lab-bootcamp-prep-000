225fa3cf3af04d2335164349221327c1f8a6e672
Tim Jordan <raeyejordan@yahoo.com>
2018-01-10 10:09:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function inc(n){
 function dec(n){
   return n-+1;
 }
-function makeInt(n,base){
-  return parseInt(n);
+function makeInt(n){
+  return parseInt(n,10);
 }
 function preserveDecimal(n){
   return parseFloat(n);
